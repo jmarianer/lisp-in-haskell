44330c9fe2af0f5b405c3a6c99d35241d75a6d13
Joey Marianer <roie.marianer@gmail.com>
2017-12-04 19:56:51 -0800

    oof

--- lisp-parse.hs
+++ lisp-parse.hs
@@ -1,31 +1,37 @@
 import Data.Char
 import Data.List
 
-data List = Atom String | List [List]
+-- Data type
+data LispObject = Atom String | List [LispObject]
 
-testInput = "(first (list 1 (+ 2 3) 9))"
-
-parseList :: String -> ([List], String)
-parseList (c:cs) = case c of
-  ' '  -> parseList (cs)
-  ')'  -> ([], cs)
-  _    -> let (firstElement, afterFirst) = parseListOrAtom
-              (restOfList, restOfInput) = parseList afterFirst
-          in  (firstElement:restOfList, restOfInput)
-  where
-    parseListOrAtom :: (List, String)
-    parseListOrAtom = case c of
-      '('  -> let (list, rest) = parseList cs                              in (List list, rest)
-      _    -> let (atom, rest) = span (\c -> c /= ' ' && c /= ')') (c:cs)  in (Atom atom, rest)
-parseList "" = ([], "")
-
-
-parseInput :: String -> List
-parseInput input = l
-  where (l:_, _) = parseList input
-
-instance Show List where
+-- Outout
+instance Show LispObject where
   show (Atom a) = a
   show (List l) = "(" ++ (intercalate " " (map show l)) ++ ")"
 
-main = putStrLn $ show $ parseInput testInput
+showIndented :: LispObject -> String
+showIndented = showIndented' ""
+showIndented' indent (Atom a) = indent ++ a ++ "\n"
+showIndented' indent (List l) = indent ++ "(\n" ++ concat (map (showIndented' (indent ++ "  ")) l) ++ indent ++ ")\n"
+
+-- Input
+parseLispObject :: String -> (LispObject, String)
+parseLispObject (c:cs) = case c of
+  '('  -> let (list, rest) = parseList cs                              in (List list, rest)
+  _    -> let (atom, rest) = span (\c -> c /= ' ' && c /= ')') (c:cs)  in (Atom atom, rest)
+
+parseList :: String -> ([LispObject], String)
+parseList (c:cs) = case c of
+  ' '  -> parseList (cs)
+  ')'  -> ([], cs)
+  _    -> let (firstElement, afterFirst) = parseLispObject (c:cs)
+              (restOfList, restOfInput) = parseList afterFirst
+          in  (firstElement:restOfList, restOfInput)
+parseList "" = ([], "")
+
+-- Very simple test
+parseInput :: String -> LispObject
+parseInput input = l
+  where (l, _) = parseLispObject input
+testInput = "(first (list 1 (+ 2 3) 9))"
+main = putStrLn $ showIndented $ parseInput testInput
