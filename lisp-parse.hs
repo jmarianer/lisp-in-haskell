26b3ff0a1527a42978086197aa255754c3e7fe1a
Joey Marianer <roie.marianer@gmail.com>
2017-12-11 14:36:21 -0800

    Basic evaluator

--- lisp-parse.hs
+++ lisp-parse.hs
@@ -29,12 +29,34 @@ parseList (c:cs) = case c of
           in  (firstElement:restOfList, restOfInput)
 parseList "" = ([], "")
 
+-- Evaluate
+data LispValue = Number Int | Function ([LispValue] -> LispValue) | ValueList [LispValue]
+instance Show LispValue where
+  show (Number i) = show i
+  show (Function _) = "A function"
+  show (ValueList l) = "(" ++ (intercalate " " (map show l)) ++ ")"
+
+fromNumber :: LispValue -> Int
+fromNumber (Number i) = i
+
+fromFunction :: LispValue -> [LispValue] -> LispValue
+fromFunction (Function f) = f
+
+eval :: LispObject -> LispValue
+eval (Atom "+") = Function (\values -> Number (sum (map fromNumber values)))
+eval (Atom "list") = Function (\list -> ValueList list)
+eval (Atom "car") = Function (\[ValueList (l:_)] -> l)
+eval (Atom string) = Number (read string)
+eval (List (function:params)) = (fromFunction (eval function)) (map eval params)
+
 -- REPL without the E :-)
 handleLine :: String -> String
 handleLine inputLine =
     "Simple show: " ++ show object ++ "\n" ++
-    "Indented show:\n" ++ showIndented' "  " object
+    "Indented show:\n" ++ showIndented' "  " object ++
+    "Evaluated: " ++ show (eval object) ++ "\n\n"
   where (object, _) = parseLispObject inputLine
+
 
 main = do
   input <- getContents
